fc63eba74fb03e845e04a3d4ebe0e48e0eabb1bf
DevBrianHQ <kipchumbabrian47@gmail.com>
2024-06-27 06:59:13 +0300

    complete lab

--- index.js
+++ index.js
@@ -1,2 +1,12 @@
+let num1 = 31;
+let num2 = 2;
+let multiply = num1 * num2
 
-// Write your code here
+let random = Math.ceil(Math.random() + 1)
+
+let num3 = 29;
+let num4 = 5;
+
+let mod =num3 % num4 
+
+let max = Math.max(2,3,4,5,7,4,5,3,20)
